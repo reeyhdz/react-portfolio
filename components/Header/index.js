dbab5b07abb25379df866f6600fef7f91aa7da06
Irving Acosta <irving.acosta@spinbyoxxo.com.mx>
2026-05-26 19:34:46 -0600

    minor fixes

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,6 @@ import Button from "../Button";
 const Header = ({ handleWorkScroll, handleAboutScroll }) => {
   return (
     <div className="mt-6 flex items-center justify-between sticky top-0 z-10 bg-white">
-      <h1 className="font-medium mob:p-2 laptop:p-0">Portafolio</h1>
       <div>
         <Button onClick={handleWorkScroll}>Trabajos</Button>
         <Button onClick={handleAboutScroll}>Acerca de</Button>
